4c2fa4c84bc191138ff0d02c0aace391a810a47e
Derek van der Mark <derek.vandermark@gmail.com>
2024-05-30 10:43:35 -0400

    Implement initial button component spec

--- client/src/components/button/button.component.tsx
+++ client/src/components/button/button.component.tsx
@@ -4,18 +4,20 @@ interface ButtonProps {
   title: string;
   onClick?: () => void;
   disabled?: boolean;
+  className?: string;
 }
 
 export default function Button({
   title,
   onClick,
   disabled,
+  className
 }: ButtonProps) {
   return (
     <button
       onClick={onClick}
       disabled={disabled}
-      className='custom-button'
+      className={`custom-button ${disabled && 'disabled'} ${className}`}
     >
       {title}
     </button> 
