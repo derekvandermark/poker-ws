5a7375e824e65cd5786b13ff8188a7377307d06c
Derek van der Mark <derek.vandermark@gmail.com>
2024-06-04 20:42:04 -0400

    Set up HTTPS server with self-signed certificate

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,47 +1,29 @@
+import https from 'https';
 import http from 'http';
 import fs from 'fs';
 import path from 'path';
 
-const hostname: string = "127.0.0.1";
+const hostname: string = '127.0.0.1';
 const port: number = 3000;
 
-const server = http.createServer((req, res) => {
-    // temporary hard coding for testing Vite with Node
+const options = {
+    key: fs.readFileSync('../../Dev-TLS-SSC/dev-key.pem'),
+    cert: fs.readFileSync('../../Dev-TLS-SSC/dev-cert.pem')
+};
 
-    // const url = new URL(req.url, `http://${req.headers.host}`);
-
-    // console.log(req.url);
-
-    // if (path.extname(url.pathname) === ".js") {
-    //     console.log("js");
-    //     res.writeHead(200, { 'content-type': 'text/javascript'});
-    //     fs.createReadStream('../client/dist/assets/index-BA-YbtxG.js').pipe(res);
-    // } 
-    // else if (path.extname(url.pathname) === ".css") {
-    //     console.log("css");
-    //     res.writeHead(200, { 'content-type': 'text/css'});
-    //     fs.createReadStream('../client/dist/assets/index-DiwrgTda.css').pipe(res);
-    // } 
-    // else if (path.extname(url.pathname) === ".svg") {
-    //     console.log("svg");
-    //     res.writeHead(200, { 'content-type': 'image/svg+xml'});
-
-    //     if (req.url === "/vite.svg") {
-    //         fs.createReadStream('../client/dist/vite.svg').pipe(res);
-    //     } else {
-    //         fs.createReadStream('../client/dist/assets/react-CHdo91hT.svg').pipe(res);
-    //     }
-    // } else {
-    //     res.writeHead(200, { 'Content-Type': 'text/html' });
-    //     fs.createReadStream('../client/dist/index.html').pipe(res);
-    // } 
-
-    // res.on('finish', () => {
-    //     console.log("here");
-    //     res.end();
-    // });
+const httpsServer = https.createServer(options, (req, res) => {
+    res.writeHead(200);
+    res.end('Hello, world!');
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
+const httpServer = http.createServer((req, res) => {
+    res.writeHead(301, { location: `https://${hostname}:${port}${req.url}` });
+    res.end();
+});
+
+httpServer.listen(3001, hostname, () => {
+    console.log('HTTP server will redirect to HTTPS');
+});
+httpsServer.listen(port, hostname, () => {
+    console.log(`HTTPS Server running at https://${hostname}:${port}`);
 });
