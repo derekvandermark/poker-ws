2c589b08bf184b49a33e423440231271603ddc03
Derek van der Mark <derek.vandermark@gmail.com>
2024-06-06 15:22:59 -0400

    Update home page UI

--- client/src/routes/root/root.tsx
+++ client/src/routes/root/root.tsx
@@ -5,6 +5,7 @@ import './root.css';
 export default function Root() {
     return (
         <main className="lobby-container" >
+            <h1 className="title" >Texas Hold'Em</h1> 
             <div className="lobby-link-container" >
                 <Link to={'create'} className="lobby-link" >Start Table</Link>
                 <Link to={'join'} className="lobby-link" >Join Table</Link>
